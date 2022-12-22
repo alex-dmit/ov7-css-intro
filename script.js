3b8009d23085c754f245a29ef67f2b29f3bd5952
Alexander Dmitriev <test@example.com>
2022-12-22 22:06:42 +0300

    Array methods

--- script.js
+++ script.js
@@ -22,3 +22,33 @@ numElem.textContent = '12'
 newDiv.append(numElem)
 document.body.append(newDiv)
 newDiv.remove()
+
+let counter = 0
+function onAddElem() {
+    const container = document.querySelector('#container')
+    const colors = ['red', 'yellow', 'black', 'green']
+    container.insertAdjacentHTML('beforeend',
+        `<div id=${Math.random()} 
+                    style="color: ${colors[Math.round(Math.random() * (colors.length - 1))]}; background-color: white;" class="elem">
+                      ${counter++}
+                      <button onclick="event.target.parentElement.remove()">X</button>
+                </div>`)
+    container.scrollIntoView({ behavior: 'smooth', block: 'end' })
+}
+function removeElem(event) {
+    event.target.parentElement.remove()
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7]
+console.log(arr.reduce((acc, cur) => {
+    return acc + cur
+}, 0))
+
+function customParseInt(str) {
+    return Number(str.split('').filter(el => !isNaN(el)).join(''))
+    // return Number(str.split('').filter((el) => {
+    //     return !isNaN(el)
+    // }).join(''))
+}
+
+console.log(customParseInt('d102f45d')) // => 1245
